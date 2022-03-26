317153ada21884f1ace40fefba996e59cddebe09
CHO WONIL <jowonil@korea.com>
2022-03-26 09:08:45 +0900

    test1

--- src/sample_ex/sample01.java
+++ src/sample_ex/sample01.java
@@ -5,6 +5,10 @@ public class sample01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("한국형 하나다");
+        System.out.println("한국형 하나다");
+        System.out.println("한국형 하나다");
+        
+        System.out.println("한국형 하나다");
 	}
 
 }
